e1330ac3ba4626b2d5496d0334d7a0d8b92df967
Charlie <cst1g16@soton.ac.uk>
2018-02-19 05:31:44 +0000

    Remove ocelots from readme

---
+++ Platformer.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame.locals import *
+
+screen = pygame.display.set_mode((500, 500))
+w = screen.get_width()
+h = screen.get_height()
+
+while True:
+    screen.fill(0)
+    pygame.display.flip()
+    for e in pygame.event.get():
+        if e.type == QUIT:
+            quit()
+        elif e.type == KEYDOWN:
+            if e.key == K_ESCAPE:
+                quit()
