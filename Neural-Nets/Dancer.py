b34c0a0c5c81f80ca428d9ac259248d4d09ed11d
Charlie <cst1g16@soton.ac.uk>
2018-04-16 16:53:32 +0100

    Attempt to implement the methods of tutorial 3 in the Dancer

--- Neural-Nets/Dancer.py
+++ Neural-Nets/Dancer.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pygame
 from pygame.locals import *
+from random import shuffle
 from time import sleep
 
 pygame.init()
@@ -17,13 +18,51 @@ pygame.init()
 # We dance!
 
 
-def sigmoid(x, deriv=False):
-    if not deriv:
-        val = 1 / (1 + np.exp(-x))
-    else:
-        val = x * (1 - x)
-    # print(val, deriv)
-    return val
+def sigmoidsquish(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def costfunction(predicted, actual, deriv=False):
+    if deriv:
+        return 2 * (predicted - actual)
+    return np.sum((predicted - actual) ** 2)
+
+
+class NeuralNet:
+    def __init__(self, *sizes):
+        self.sizes = sizes
+        self.synno = len(sizes) - 1
+        self.synweights = []
+        self.synbiases = []
+        for i in range(self.synno):
+            self.synweights.append(2 * np.random.random((sizes[i], sizes[i + 1])) - 1)
+            self.synbiases.append(2 * np.random.random(sizes[i + 1]) - 1)
+    
+    def use(self, inputdata):
+        layer = np.array(inputdata.copy())
+        layers = [layer]
+        for i in range(self.synno):
+            layer = sigmoidsquish(layer.dot(self.synweights[i]) + self.synbiases[i])
+            layers.append(layer)
+        # print(layer)
+        return layers
+    
+    def train(self, trainingdata):
+        if len(trainingdata) > 0:
+            np.random.shuffle(trainingdata)
+            for training in trainingdata:
+                prediction = self.use(training[0])
+                # print(inputdata, "->", prediction[-1])
+                # print("Should be:", outputdata)
+                cost = costfunction(prediction[-1], training[1])
+                nudge = -costfunction(prediction[-1], training[1], True)
+                # print("How bad? {} bad.".format(cost))
+                for i in range(-1, -(self.synno + 1), -1):
+                    # print("Nudges required:", nudge)
+                    self.synbiases[i] += nudge
+                    newnudge = np.dot(self.synweights[i], nudge)
+                    self.synweights[i] += (np.array([prediction[i - 1]]).T.dot(np.array([nudge])))
+                    nudge = newnudge
 
 
 screen = pygame.display.set_mode((1000, 500))
@@ -36,11 +75,31 @@ white = (255, 255, 255)
 turquoise = (0, 255, 255)
 pink = (255, 0, 255)
 
-recordlength = 1
+recordlength = 3
+recordslength = 50
+
+trainingdata = [[0,0,0,1],
+                [0,0,1,0],
+                [0,0,1,1],
+                [0,1,0,0],
+                [0,1,0,1],
+                [0,1,1,0],
+                [0,1,1,1],
+                [1,0,0,0],
+                [1, 0, 0, 1],
+                [1, 0, 1, 0],
+                [1, 0, 1, 1],
+                [1, 1, 0, 0],
+                [1, 1, 0, 1],
+                [1, 1, 1, 0],
+                [1, 1, 1, 1]]
+
+trainingdata = [[i * recordlength, i * recordlength] for i in trainingdata]
 
 gravity = np.float32([0, 0.5])
 
 balls = []
+
 
 class Ball:
     def __init__(self, pos, mass=50, col=white):
@@ -64,17 +123,15 @@ class PlayerBall(Ball):
         pos = np.float32(pos)
         self.posrecord = []
         super(PlayerBall, self).__init__(pos, mass, col)
-        for i in range(recordlength):
-            for j in range(len(self.pos)):
-                self.posrecord.append(self.pos[j] - size[j])
+        self.posrecord += [False] * recordlength * 4
     
-    def move(self, keys):
+    def move(self, keys, opposition):
         # print(keys)
         keycheck = [(key in keys) for key in self.keys]
         rightforce = keycheck[3] - keycheck[1]
         downforce = keycheck[2] - keycheck[0]
         self.kick((rightforce, downforce))
-        self.kick(gravity)
+        # self.kick(gravity)
         self.vel += self.acc
         self.pos += self.vel
         if self.pos[0] < 0 or self.pos[0] >= w:
@@ -83,47 +140,55 @@ class PlayerBall(Ball):
             self.vel[1] = 0
         self.pos = np.clip(self.pos, np.zeros(2), np.array([w, h]))
         self.acc = np.zeros(2, 'float32')
-        for i in range(len(self.pos)):
-            self.posrecord.append(self.pos[i] - size[i])
+        self.posrecord.append(opposition.pos[1] <= self.pos[1])
+        self.posrecord.append(opposition.pos[0] <= self.pos[0])
+        self.posrecord.append(opposition.pos[1] >= self.pos[1])
+        self.posrecord.append(opposition.pos[0] >= self.pos[0])
+        while len(self.posrecord) > (recordlength * 4):
             self.posrecord.pop(0)
         # print(self.posrecord)
         # print(keycheck)
-        return np.array([self.posrecord]), np.array([keycheck])
+        return self.posrecord, keycheck
 
 
 class AIBall(PlayerBall):
     def __init__(self, pos, mass=50, col=turquoise):
-        self.syn0 = 2 * np.random.random((recordlength * 2, recordlength * 4)) - 1
-        self.syn1 = 2 * np.random.random((recordlength * 4, 4)) - 1
+        self.brain = NeuralNet(recordlength * 4,
+                               recordlength * 8,
+                               4)
         super(AIBall, self).__init__(pos, mass=mass, col=col)
     
-    def move(self, inputdata, outputdata):
-        layer0 = np.float32(inputdata.copy())
-        layer1 = sigmoid(np.dot(layer0, self.syn0))
-        layer2 = sigmoid(np.dot(layer1, self.syn1))
-        layer2error = outputdata - layer2
-        layer2delta = layer2error * sigmoid(layer2, True)
-        layer1error = layer2delta.dot(self.syn1.T)
-        layer1delta = layer1error * sigmoid(layer1, True)
-        self.syn1 += layer1.T.dot(layer2delta)
-        self.syn0 += layer0.T.dot(layer1delta)
-
-        layer0 = np.float32(self.posrecord.copy())
-        layer1 = sigmoid(np.dot(layer0, self.syn0))
-        layer2 = sigmoid(np.dot(layer1, self.syn1))
-        activations = layer2 > 0.9
-        super(AIBall, self).move(self.keys[activations])
+    def move(self, opposition):
+        activations = self.brain.use(self.posrecord)[-1] > 0.5
+        # print(activations)
+        return super(AIBall, self).move(self.keys[activations], opposition)
 
 
-AIs = [AIBall((w/2, h/2)) for i in range(5)]
+AIs = [AIBall((w/4, h/4)) for i in range(5)]
+for i in range(5000):
+    for AI in AIs:
+        AI.brain.train(trainingdata)
 Player = PlayerBall((w/2, h/2))
 keyset = set()
+istraining = True
+records = []
+record = trainingdata[0]
 
+cycles = -1
 while True:
     screen.fill(0)
-    record = Player.move(keyset)
+    cycles += 1
+    Player.move(keyset, AI)
+    # if istraining:
+    #     records.append(record)
+    # if len(records) > recordslength:
+    #     AI.brain.train(records)
+    #     if istraining:
+    #         records.pop(0)
     for AI in AIs:
-        AI.move(*record)
+        if istraining:
+            AI.brain.train([record])
+        record = AI.move(Player)
     for B in balls:
         B.show()
     pygame.display.flip()
@@ -134,6 +199,8 @@ while True:
             keyset.add(e.key)
             if e.key == K_ESCAPE:
                 quit()
+            if e.key == K_SPACE:
+                istraining = not istraining
         elif e.type == KEYUP:
             keyset.remove(e.key)
-    #sleep(0.01)
+    sleep(0.01)
