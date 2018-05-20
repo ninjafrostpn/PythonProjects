d63d2410aecb5a2877f74227b9e37d2e9447373d
Charlie <cst1g16@soton.ac.uk>
2018-05-20 04:38:55 +0100

    Add spooky skull embossing

--- Glitcheroony/Glitcheroony.py
+++ Glitcheroony/Glitcheroony.py
@@ -26,7 +26,7 @@ def bars(frame, barwidth=30, barno=10):
 
 
 def recolor(frame, proportions=(-1, -1, -1)):
-    proportions = np.float32([(proportions[i] if proportions[i] > -1 else r.random() * 1.5) for i in range(3)])
+    proportions = np.float32([(proportions[i] if proportions[i] > -1 else r.random() + 0.5) for i in range(3)])
     frame[:, :] = np.minimum(np.maximum(np.int32(frame[:, :] * np.float32(proportions)), 0), 255)
     
 
@@ -40,6 +40,18 @@ def jitter(frame, maxjitterdist=10, jitterno=1):
         xj = r.randrange(-maxjitterdist, maxjitterdist)
         yj = r.randrange(-maxjitterdist, maxjitterdist)
         frame[y1 + yj: y2 + yj, x1 + xj: x2 + xj] = frame[y1: y2, x1: x2]
+        
+        
+def emboss(frame, mask, embossdist=(0, 50)):
+    framedims = frame.shape[:2]
+    maskdims = mask.shape[:2]
+    left = r.randrange(embossdist[0], framedims[1] - maskdims[1] - embossdist[0])
+    top = r.randrange(embossdist[1], framedims[0] - maskdims[0] - embossdist[1])
+    right = left + maskdims[1]
+    bottom = top + maskdims[0]
+    xj = embossdist[0]
+    yj = embossdist[1]
+    frame[top + yj: bottom + yj, left + xj: right + xj][mask] = frame[top: bottom, left: right][mask]
 
 
 sleep(2)
@@ -49,11 +61,16 @@ cv2.namedWindow(windowname, cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty(windowname, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 cv2.startWindowThread()
 
-# Hey look, a plainext password!
-passcode = [103, 101, 116, 109, 101, 111, 117, 116]  # getmeout
+# Hey look, a plaintext password!
+passcode = [103, 101, 116, 109, 101, 111, 117, 116, 27]  # getmeout[esc]
 checkcode = []
 
+flatmask = cv2.imread(r"D:\Users\Charles Turvey\Pictures\Art\Shadows\Skull.png")
+flatmask = cv2.cvtColor(flatmask, cv2.COLOR_RGB2BGR)
+flatmask = flatmask[:, :, 0] == 0
+
 cycles = 0
+started = False
 while True:
     key = cv2.waitKey(33)
     if key != -1:
@@ -64,6 +81,9 @@ while True:
             print("Not", checkcode)
             checkcode = []
     if cycles > 200:
+        if not started:
+            emboss(frame, flatmask)
+            started = True
         if r.random() >= 0.5:
             bars(frame, barno=r.randrange(10), barwidth=5)
         else:
@@ -72,6 +92,7 @@ while True:
         if cycles == 200:
             recolor(frame)
             cycles %= 200
+            started = False
     else:
         frame = screengrab.copy()
         cycles += r.randrange(10)
