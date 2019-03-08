81be9c6e9e7fc614f15f5c93326057c7e3ab064f
ninjafrostpn <cst1g16@soton.ac.uk>
2019-03-08 14:10:35 +0100

    Add pin placer for points on trace

--- Tools/LSSSListComScatter_to_KML.py
+++ Tools/LSSSListComScatter_to_KML.py
@@ -1,65 +1,72 @@
 import os
 
-blankdocument = """<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
-<Document>
-	<name>{5}.kml</name>
-	<StyleMap id="m_ylw-pushpin">
-		<Pair>
-			<key>normal</key>
-			<styleUrl>#s_ylw-pushpin</styleUrl>
-		</Pair>
-		<Pair>
-			<key>highlight</key>
-			<styleUrl>#s_ylw-pushpin_hl</styleUrl>
-		</Pair>
-	</StyleMap>
-	<Style id="s_ylw-pushpin">
-		<IconStyle>
-			<scale>1.1</scale>
-			<Icon>
-				<href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>
-			</Icon>
-			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
-		</IconStyle>
-		<LineStyle>
-			<color>{1}</color>
-			<width>{2}</width>
-		</LineStyle>
-	</Style>
-	<Style id="s_ylw-pushpin_hl">
-		<IconStyle>
-			<scale>1.3</scale>
-			<Icon>
-				<href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>
-			</Icon>
-			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
-		</IconStyle>
-		<LineStyle>
-			<color>{1}</color>
-			<width>{2}</width>
-		</LineStyle>
-	</Style>
-	<Placemark>
-		<name>{0}</name>
-		<description>{3}</description>
-		<styleUrl>#m_ylw-pushpin</styleUrl>
-		<LineString>
-			<tessellate>1</tessellate>
-			<altitudeMode>absolute</altitudeMode>
-			<coordinates>
-				{4}
-			</coordinates>
-		</LineString>
-	</Placemark>
-</Document>
-</kml>
-"""
+blankdocument = ('<?xml version="1.0" encoding="UTF-8"?>'
+                 '<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">'
+                 '<Document>\n'
+                 '    <name>{5}.kml</name>\n'
+                 '    <StyleMap id="m_ylw-pushpin">\n'
+                 '        <Pair>\n'
+                 '            <key>normal</key>\n'
+                 '            <styleUrl>#s_ylw-pushpin</styleUrl>\n'
+                 '        </Pair>\n'
+                 '        <Pair>\n'
+                 '            <key>highlight</key>\n'
+                 '            <styleUrl>#s_ylw-pushpin_hl</styleUrl>\n'
+                 '        </Pair>\n'
+                 '    </StyleMap>\n'
+                 '    <Style id="s_ylw-pushpin">\n'
+                 '        <IconStyle>\n'
+                 '            <scale>1.1</scale>\n'
+                 '            <Icon>\n'
+                 '                <href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>\n'
+                 '            </Icon>\n'
+                 '            <hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>\n'
+                 '        </IconStyle>\n'
+                 '        <LineStyle>\n'
+                 '            <color>{1}</color>\n'
+                 '            <width>{2}</width>\n'
+                 '        </LineStyle>\n'
+                 '    </Style>\n'
+                 '    <Style id="s_ylw-pushpin_hl">\n'
+                 '        <IconStyle>\n'
+                 '            <scale>1.3</scale>\n'
+                 '            <Icon>\n'
+                 '                <href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>\n'
+                 '            </Icon>\n'
+                 '            <hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>\n'
+                 '        </IconStyle>\n'
+                 '        <LineStyle>\n'
+                 '            <color>{1}</color>\n'
+                 '            <width>{2}</width>\n'
+                 '        </LineStyle>\n'
+                 '    </Style>\n'
+                 '    <Placemark>\n'
+                 '        <name>{0}</name>\n'
+                 '        <description>{3}</description>\n'
+                 '        <styleUrl>#m_ylw-pushpin</styleUrl>\n'
+                 '        <LineString>\n'
+                 '            <tessellate>1</tessellate>\n'
+                 '            <altitudeMode>absolute</altitudeMode>\n'
+                 '            <coordinates>\n'
+                 '                {4}\n'
+                 '            </coordinates>\n'
+                 '        </LineString>\n'
+                 '    </Placemark>\n'
+                 '{6}'
+                 '</Document>\n'
+                 '</kml>')
+
+blankpin = ('  <Placemark>\n'
+            '    <name>{1}</name>'
+            '    <description>{2}</description>\n'
+            '    <styleUrl>#pointstyle</styleUrl>\n'
+            '    <Point>\n'
+            '      <coordinates>{0},0</coordinates>\\nn'
+            '    </Point>\n'
+            '  </Placemark>\n')
 
 
 def fromLCS(rawLCS):
-    lats = []
-    lons = []
     pts = []
     Npos = 0
     Epos = 0
@@ -69,10 +76,9 @@ def fromLCS(rawLCS):
             if rawLCS[Npos + 1] == 'A':
                 Npos = rawLCS.index("  N", Npos + 5) + 2
             Epos = rawLCS.index(" E", Npos) + 1
-            lats.append(str(int(rawLCS[Npos + 1: Npos + 3]) + (float(rawLCS[Npos + 4: Npos + 8]) / 60)))
-            lons.append(str(int(rawLCS[Epos + 1: Epos + 4]) + (float(rawLCS[Epos + 5: Epos + 9]) / 60)))
-            pts.append((lons[-1], lats[-1]))
-            print((lons[-1], lats[-1]))
+            pts.append((str(int(rawLCS[Epos + 1: Epos + 4]) + (float(rawLCS[Epos + 5: Epos + 9]) / 60)),
+                        str(int(rawLCS[Npos + 1: Npos + 3]) + (float(rawLCS[Npos + 4: Npos + 8]) / 60)),
+                        rawLCS[Npos: Epos + 9]))
         except:
             break
     return pts
@@ -83,9 +89,11 @@ def toKML(pts, name="", filename=None, col=(255, 165, 0), width=1, desc=""):
         filename = name
     if filename.endswith(".txt"):
         filename = filename[:-4]
+    filename.replace(".", "-")
     hexcol = "ff" + hex((((col[0] << 8) + col[1]) << 8) + col[1])[2:]
-    ptstring = " ".join([",".join([str(float(i)) for i in pt]) for pt in pts])
-    return blankdocument.format(name, hexcol, width, desc, ptstring, filename)
+    ptstring = " ".join([",".join([str(float(i)) for i in pt[:2]]) for pt in pts])
+    pinstring = "".join([blankpin.format(",".join([str(c) for c in pt[:2]]), i, pt[2]) for i, pt in enumerate(pts)])
+    return blankdocument.format(name, hexcol, width, desc, ptstring, filename, pinstring)
 
 
 def readfile(filepath):
