7b85b63cc69c22a6880617f0d3f13d6b0d4a7f74
ninjafrostpn <cst1g16@soton.ac.uk>
2019-04-01 17:23:37 +0200

    Add shoal stats to actual 3D version of shoal simulator

--- BehaviouralEcology/Shoal/CouzinShoalActual3D.py
+++ BehaviouralEcology/Shoal/CouzinShoalActual3D.py
@@ -3,34 +3,46 @@ from mpl_toolkits import mplot3d
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from time import sleep
 
-debug = False  # If True, shows zones of detection and allows their alteration with mouse position
+boxedin = True
+showfish = True
 matplotlib.interactive(True)
 
 # Interface initialisation
-w = 25
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlim(0, w)
-ax.set_ylim(w, 0)
-ax.set_zlim(0, w)
-ax.invert_zaxis()
-keys = set()  # For keypress detection
+w = np.float32([200, 200, 200])
+if showfish:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    if boxedin:
+        ax.set_xlim(0, w[0])
+        ax.set_ylim(w[1], 0)
+        ax.set_zlim(0, w[2])
+    ax.invert_zaxis()
+else:
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax2 = fig.add_subplot(2, 1, 2, sharex=ax1)
+    plt.draw()
 
 # Set simulation parameters
 N = 100       # Number of fish
-s = 3         # Speed of fish (px per time unit)
+s = 10         # Speed of fish (px per time unit)
 T = 0.1       # Timestep (time units per cycle)
 alpha = 270   # Visual range of fish (degrees, centred on front of fish)
 theta = 40    # Turning speed of fish (degrees per time unit)
 sigma = 0.05  # Error
-r_r = 1       # Outer radius of Zone of Repulsion
-r_o = 3       # Outer radius of Zone of Orientation
-r_a = 17      # Outer radius of Zone of Attraction
+r_r = 2       # Outer radius of Zone of Repulsion
+r_o_d = 4    # Width of Zone of Orientation
+r_a_d = 28    # Width of Zone of Attraction
 
 # The position vectors, c, and unit direction vectors, v, of the fish
-c = (0.5 + np.random.random_sample((N, 3))) * w / 2  # Initialise positions in middle 1/8 of volume
+c = np.random.random_sample((N, 3)) * w
+while True:
+    mask_c_bad = np.linalg.norm(c - w/2, axis=1) > r_a_d
+    if np.any(mask_c_bad):
+        c[mask_c_bad] = np.random.random_sample((np.sum(mask_c_bad), 3)) * w
+    else:
+        break
 v = np.float32([np.sin([ang + np.pi/2, ang, ang - np.pi/2]) for ang in (np.random.random_sample(N) * (np.pi * 2))])
 v = (v.T / np.linalg.norm(v, axis=1)).T  # Normalisation. There's a lot of this in this program.
 
@@ -39,11 +51,20 @@ v = (v.T / np.linalg.norm(v, axis=1)).T  # Normalisation. There's a lot of this 
 # - pairs[1] are used as the fish being detected, generally called fish j
 pairs = np.nonzero(np.tri(N, dtype="bool") ^ ~np.tri(N, k=-1, dtype="bool"))
 
-# Select random colours
-colours = np.random.random_sample((N, 3))
-# Cater for weird render order of arrow parts
-colours = np.append(colours, [colours[int(i/2)] for i in range(N * 2)], axis=0)
+if showfish:
+    # Select random colours
+    colours = np.random.random_sample((N, 3))
+    # Cater for weird render order of arrow parts
+    colours = np.append(colours, [colours[int(i/2)] for i in range(N * 2)], axis=0)
 
+
+T_total = 0
+recording = False
+r_o_d_records = []
+p_group_records = []
+m_group_records = []
+print("Start Simulation")
+print("Stabilisation start for", r_o_d)
 while True:
     # The vector pointing from each fish i to each fish j
     r_ij = c[pairs[1]] - c[pairs[0]]         # Full vector
@@ -64,24 +85,29 @@ while True:
     # - a tank wall in their repulsion zone (this is not in the model as per the paper)
     # - any fish in their repulsion zone
     mask_mode_r = np.zeros(r_ij_abs.shape, dtype="bool")
-    # Masks (for the list of all fish i) that single out:
-    mask_toplft = np.any(c < r_r, axis=1)      # those close to the top or left walls
-    mask_btmrgt = np.any(c > w - r_r, axis=1)  # those close to the bottom or right walls
-    # Fills in mask_mode_r with all the fish satisfying any of these conditions
-    for i in set(pairs[0][mask_zor]):
-        mask_mode_r |= (pairs[0] == i) | mask_toplft[pairs[0]] | mask_btmrgt[pairs[0]]
+    if boxedin:
+        # Masks (for the list of all fish i) that single out:
+        mask_toplft = np.any(c < r_r, axis=1)      # those close to the top or left walls
+        mask_btmrgt = np.any(c > w - r_r, axis=1)  # those close to the bottom or right walls
+        # Fills in mask_mode_r with all the fish satisfying any of these conditions
+        for i in set(pairs[0][mask_zor]):
+            mask_mode_r |= (pairs[0] == i) | mask_toplft[pairs[0]] | mask_btmrgt[pairs[0]]
+    else:
+        for i in set(pairs[0][mask_zor]):
+            mask_mode_r |= (pairs[0] == i)
     # Masks (for the pair arrays) that single out pairings where fish j is in fish i's
-    mask_zoo = mask_visible & ~mask_mode_r & (r_ij_abs < r_o)              # Zone of Orientation
-    mask_zoa = mask_visible & ~mask_mode_r & ~mask_zoo & (r_ij_abs < r_a)  # Zone of Attraction
+    mask_zoo = mask_visible & ~mask_mode_r & (r_ij_abs < r_r + r_o_d)                      # Zone of Orientation
+    mask_zoa = mask_visible & ~mask_mode_r & ~mask_zoo & (r_ij_abs < r_r + r_o_d + r_a_d)  # Zone of Attraction
 
     # Generating the unit direction vectors representing the direction fish i would like to go
     d_i = np.zeros((N, 3))
     # Generating unit direction vectors for repulsion
     d_r = np.zeros((N, 3))
     d_r[pairs[0][mask_zor]] -= r_ij_norm[mask_zor]
-    # (These must be redone separately to mask_topleft etc, else the fish move on a certain diagonal away from walls)
-    d_r[c < r_r] += 1
-    d_r[c > w - r_r] -= 1
+    if boxedin:
+        # (These must be redone separately to mask_topleft etc else the fish move on a certain diagonal away from walls)
+        d_r[c < r_r] += 1
+        d_r[c > w - r_r] -= 1
     d_r[pairs[0][mask_zor]] = (d_r[pairs[0][mask_zor]].T / np.linalg.norm(d_r[pairs[0][mask_zor]], axis=1)).T
     d_i += d_r
     # Generating unit direction vectors for orientation
@@ -144,12 +170,69 @@ while True:
     v[~mask_close & ~mask_zeroes] = v_new
 
     c += v * s * T  # Movement of all fish i in the direction of v at speed s over one timestep, T
-    c = np.minimum(np.maximum(c, 0), w)  # But stop them at the screen edge
+    if boxedin:
+        c = np.minimum(np.maximum(c, 0), w)  # But stop them at the screen edge
+
+    T_total += T
+
+    # Obtain shoal metrics
+    c_group = np.average(c, axis=0)
+    d_group = np.average(v, axis=0)
+    p_group = np.linalg.norm(np.sum(v, axis=0)) / N
+    r_ic = c - c_group
+    m_group = np.linalg.norm(np.sum(np.cross(v, r_ic), axis=0)) / N
 
     # Drawing things
-    drawnarrows = ax.quiver(*c.T, *v.T, color=colours,
-                            pivot="tip", length=w/10, arrow_length_ratio=0.5)
-
-    plt.draw()
-    fig.canvas.flush_events()
-    drawnarrows.remove()
+    if showfish:
+        if not boxedin:
+            r_max = np.max(np.abs(r_ic))
+            top = c_group + r_max
+            btm = c_group - r_max
+            ax.set_xlim(btm[0], top[0])
+            ax.set_ylim(top[1], btm[1])
+            ax.set_zlim(btm[2], top[2])
+        drawnarrows = ax.quiver(*c.T,
+                                *v.T,
+                                color=colours,
+                                pivot="tip", length=min(w)/10, arrow_length_ratio=0.5)
+        plt.draw()
+        fig.canvas.flush_events()
+        drawnarrows.remove()
+    else:
+        if T_total > 500:
+            recording = not recording
+            if recording:
+                print("Recording start for", r_o_d)
+                p_group_total = p_group
+                m_group_total = m_group
+            else:
+                print("Recording stop for", r_o_d)
+                print(r_o_d, p_group_total / T_total, m_group_total / T_total)
+                r_o_d_records.append(r_o_d)
+                p_group_records.append(p_group_total / T_total)
+                m_group_records.append(m_group_total / T_total)
+                scat1 = ax1.scatter(r_o_d_records, p_group_records)
+                scat2 = ax2.scatter(r_o_d_records, m_group_records)
+                plt.draw()
+                fig.canvas.flush_events()
+                scat1.remove()
+                scat2.remove()
+                r_o_d += 0.1
+                r_o_d %= 5
+                # Reset
+                c = np.random.random_sample((N, 3)) * w
+                while True:
+                    mask_c_bad = np.linalg.norm(c - w / 2, axis=1) > r_a_d
+                    print(sum(mask_c_bad))
+                    if np.any(mask_c_bad):
+                        c[mask_c_bad] = np.random.random_sample((np.sum(mask_c_bad), 3)) * w
+                    else:
+                        break
+                v = np.float32([np.sin([ang + np.pi / 2, ang, ang - np.pi / 2]) for ang in
+                                (np.random.random_sample(N) * (np.pi * 2))])
+                v = (v.T / np.linalg.norm(v, axis=1)).T
+                print("Stabilisation start for", r_o_d)
+            T_total %= 500
+        elif recording:
+            p_group_total += p_group
+            m_group_total += m_group
