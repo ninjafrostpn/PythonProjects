f22591d8f64f4781d8a37cd63f9b50d8326152e5
ninjafrostpn <cst1g16@soton.ac.uk>
2019-12-14 23:16:21 +0000

    Get drawing of material working for thin upward peaks

--- Bending/Bending 2.py
+++ Bending/Bending 2.py
@@ -6,3 +6,108 @@
 # Lowest density is 1, 0 indicates absence of material
 # (possibly negative numbers for places the environment is drawn to; might help calculation)
 # Higher-density pixels can be drawn out into several lower ones
+
+import pygame
+from pygame.locals import *
+import numpy as np
+from time import sleep
+
+pygame.init()
+
+coltonum = lambda r, g, b: (((r << 8) + g) << 8) + b
+
+adjvecs = np.int32([[i, j]
+                    for i in range(-1, 2)
+                    for j in range(-1, 2)
+                    if 0 < abs(i + j) < 2])
+
+w, h = 500, 250
+screensize = np.int32((w, h))
+smolscreen = pygame.Surface(screensize)
+screen = pygame.display.set_mode(screensize * 2)
+
+sandgrid = np.zeros(screensize)
+smolgrid = pygame.surfarray.pixels3d(smolscreen)
+# print("W", sandgrid[tuple((adjvecs + (10, 10)).T)])
+
+sandgrid[:, -100:] = 10
+sandgrid[200, 100] = -3
+print(sandgrid[200, 100])
+
+
+def drawin(cent, checked=None, depth=0, debug=False):
+    if debug:
+        print(depth, ": ", cent, sep="")
+    if checked is None:
+        checked = []
+    if tuple(cent) not in checked:
+        checked.append(tuple(cent))
+    adj = np.int32(cent) + adjvecs
+    np.random.shuffle(adj)
+    nextchecks = []
+    for a in adj:
+        if tuple(a) not in checked:
+            checked.append(tuple(a))
+            aval = sandgrid[tuple(a)]
+            if aval > 1:
+                if debug:
+                    print(depth + 0.5, ": ", a, sep="")
+                return a
+            elif aval > 0:
+                nextchecks.append(a)
+    if len(nextchecks) > 0:
+        np.random.shuffle(nextchecks)
+        for a in nextchecks:
+            b = drawin(a, checked, depth + 1, debug)
+            if b is not None:
+                if debug:
+                    print(depth + 0.5, ": ", b, sep="")
+                return b
+    return None
+
+
+keys = set()
+
+while True:
+    smolscreen.fill(0)
+    for pos in np.argwhere(sandgrid < 0):
+        # North: -1, East: -2, South: -3, West: -4
+        val = sandgrid[tuple(pos)]
+        # print(pos, val)
+        # if val == -1:
+        #     check = np.argwhere(sandgrid[pos[0], pos[1]-1::-1] > 0)
+        # if val == -2:
+        #     check = np.argwhere(sandgrid[pos[0]+1:, pos[1]] > 0)
+        if val == -3:
+            check = np.argwhere(sandgrid[pos[0], pos[1]+1:] > 0)
+        # if val == -4:
+        #     check = np.argwhere(sandgrid[pos[0]-1::-1, pos[1]] > 0)
+        if len(check) > 0:
+            found = check[0]
+            if val == -3:
+                tocell = pos + [0, found]
+            fromcell = drawin(tocell)
+            if fromcell is not None:
+                sandgrid[tuple(fromcell)] -= 1
+                sandgrid[tuple(tocell)] += 1
+    smolgrid[sandgrid > 0, 2] = 255 - (10 * sandgrid[sandgrid > 0])
+    smolgrid[sandgrid < 0, :] = 255
+    pygame.transform.scale2x(smolscreen, screen)
+    pygame.display.flip()
+    mousepos = np.int32(pygame.mouse.get_pos())
+    for e in pygame.event.get():
+        if e.type == QUIT:
+            quit()
+        elif e.type == KEYDOWN:
+            keys.add(e.key)
+            if e.key == K_ESCAPE:
+                quit()
+        elif e.type == KEYUP:
+            keys.discard(e.key)
+        elif e.type == MOUSEBUTTONDOWN:
+            clickpos = tuple(np.int32(mousepos / 2))
+            print(clickpos, sandgrid[clickpos])
+            if sandgrid[clickpos] == 0:
+                sandgrid[sandgrid < 0] = 0
+                sandgrid[clickpos] = -3
+    sleep(0.1)
