10a3fb205e7c610a000b588570a9643eef19b34c
ninjafrostpn <cst1g16@soton.ac.uk>
2020-01-11 20:23:01 +0000

    Do some really rudimentary collision resolution

--- Bending/Bending 3.py
+++ Bending/Bending 3.py
@@ -32,18 +32,22 @@ movemode = 0
 enough = 100
 threshold = 0
 
-pos = np.int32([np.tile(np.arange(0, 100, 1, "int32"), 100), np.repeat(np.arange(0, 100, 1, "int32"), 100)]).T
-mass = np.random.randint(1, 4, pos.shape[0])
-vel = np.random.randint(0, 5, pos.shape, "int32")
+nx = 10
+pos = np.int32([[*np.tile(np.arange(0, nx, 1, "int32"), 50), *np.tile(np.arange(300 - nx, 300, 1, "int32"), 50)],
+                [*np.repeat(np.arange(0, 50, 1, "int32"), nx), *np.repeat(np.arange(0, 50, 1, "int32"), nx)]]).T
+mass = np.ones(pos.shape[0], "int32")
+vel = np.zeros(pos.shape, "int32")
+vel[:pos.shape[0] // 2] = 20
+mass[pos.shape[0] // 2:] = 10
 force = np.zeros(pos.shape, "int32")
 
 while True:
     screen.fill(0)
     screengrid[pos[:, 0], pos[:, 1], 0] = 255 - ((mass - 1) * 10)
-    try:
-        screengrid[colls[:, 0], colls[:, 1], 1] = 255
-    except NameError:
-        pass
+    # try:
+    #     screengrid[collpos[:, 0], collpos[:, 1], 1] = 255
+    # except NameError:
+    #     pass
     for i in range(2):
         for j in range(2):
             windowgrid[i::2, j::2] = screengrid
@@ -58,7 +62,7 @@ while True:
                 quit()
         elif e.type == KEYUP:
             keys.discard(e.key)
-    # time.sleep(1)
+    # time.sleep(mousepos[0] / (10 * w))
 
     # In order to physics in discrete space, subdivide each tick according to lcm of integer speeds of objects
     #   Each object moves only in steps of 1 in x or y
@@ -77,7 +81,7 @@ while True:
     #           The velocity composition (and hence lcm) may change with collisions
     #       Packed, resting objects will collide... a lot, which could be slow
     #           Maybe keep track of columns/rows locked against the sides?
-    uniquev = np.unique(vel)
+    uniquev = np.unique(np.abs(vel))
     tickdiv = np.max(uniquev)
     # print(uniquev, tickdiv)
     # https://stackoverflow.com/a/42472824
@@ -85,12 +89,42 @@ while True:
         # print(v, tickdiv, np.gcd(tickdiv, v))
         tickdiv = int(tickdiv * v / np.gcd(tickdiv, v))
     # print("F", tickdiv)
-    for i in range(1, tickdiv + 1):
+    i = 1
+    while i < tickdiv:
         movers = ((tickdiv / i) % np.abs(vel)) == 0
         # print(i, tickdiv, vel[movers])
         pos[movers] += np.sign(vel[movers])
+        vel[((pos == 0) & (vel < 0)) | ((pos == (w, h)) & (vel > 0))] *= -1
         pos = np.minimum(np.maximum(0, pos), (w - 1, h - 1))
         # https://stackoverflow.com/q/11528078
         a = np.lexsort([*pos.T])
-        # print(pos[a[0]], pos[a[-1]])
-        colls = pos[a[:-1][np.all(pos[a[:-1]] == pos[a[1:]], axis=1)]]
+        coll = np.all(pos[a[:-1]] == pos[a[1:]], axis=1)
+        # print(sum(coll))
+        if np.any(coll):
+            collpos = pos[a[:-1][coll]]
+            # print(pos[a[0]], pos[a[-1]])
+            # Assume that there are only 2 parties to each collision, and that all masses are equal...
+            oppcoll = (vel[a[:-1][coll]] * vel[a[1:][coll]]) < 0
+            # print(sum(oppcoll))
+            newvel = vel.copy()
+            # print(vel.shape, a.shape, oppcoll.shape)
+            newvel[a[:-1][coll][oppcoll[:, 0]], 0] = vel[a[1:][coll][oppcoll[:, 0]], 0]
+            newvel[a[:-1][coll][oppcoll[:, 1]], 1] = vel[a[1:][coll][oppcoll[:, 1]], 1]
+            newvel[a[1:][coll][oppcoll[:, 0]], 0] = vel[a[:-1][coll][oppcoll[:, 0]], 0]
+            newvel[a[1:][coll][oppcoll[:, 1]], 1] = vel[a[:-1][coll][oppcoll[:, 1]], 1]
+
+            newvel[a[:-1][coll][~oppcoll[:, 0]], 0] = vel[a[1:][coll][~oppcoll[:, 0]], 0]
+            newvel[a[:-1][coll][~oppcoll[:, 1]], 1] = vel[a[1:][coll][~oppcoll[:, 1]], 1]
+            newvel[a[1:][coll][~oppcoll[:, 0]], 0] = vel[a[:-1][coll][~oppcoll[:, 0]], 0]
+            newvel[a[1:][coll][~oppcoll[:, 1]], 1] = vel[a[:-1][coll][~oppcoll[:, 1]], 1]
+            # print(time.time(), np.all(newvel == vel), newvel is vel)
+            vel[:, :] = newvel[:, :]
+            if not np.all((tickdiv % vel) == 0):
+                p = i / tickdiv
+                uniquev = np.unique(np.abs(vel))
+                for v in uniquev[(tickdiv % uniquev) != 0]:
+                    tickdiv = int(tickdiv * v / np.gcd(tickdiv, v))
+                # print(tickdiv)
+                i = np.floor(p * tickdiv)
+        i += 1
+    # vel -= np.int32(np.ceil(vel / 100))
